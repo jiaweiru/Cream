4d147cc5db4021b3af3edca7f8a662d8040e9e55
jwru <jiawei.ru@qq.com>
2025-09-16 18:12:49 +0800

    update something

--- cream/audio/processing.py
+++ cream/audio/processing.py
@@ -6,10 +6,11 @@ from pathlib import Path
 import librosa
 import soundfile as sf
 
-from cream.core.processor import register_processor
+from cream.core.processor import register_processor, ModelBackedProcessor
 from cream.audio.audio_processor import BaseAudioProcessor
 from cream.core.exceptions import AudioProcessingError
 from cream.core.logging import get_logger
+from cream.core.config import config
 
 logger = get_logger(__name__)
 
@@ -21,7 +22,6 @@ class AudioResampler(BaseAudioProcessor):
     ) -> Path:
         """Resample audio to target sample rate."""
         self.validate_input(input_path)
-
         output_path = output_path or input_path
         if "target_sr" not in kwargs:
             logger.warning(
@@ -44,7 +44,6 @@ class AudioNormalizer(BaseAudioProcessor):
         learn more: https://github.com/slhck/ffmpeg-normalize
         """
         self.validate_input(input_path)
-
         output_path = output_path or input_path
         normalization_type = kwargs.get("normalization_type")
         target_level = kwargs.get("target_level")
@@ -63,3 +62,59 @@ class AudioNormalizer(BaseAudioProcessor):
             logger.error(f"Command failed: {e}\nstdout: {e.stdout}\nstderr: {e.stderr}")
             raise AudioProcessingError from e
         return output_path or input_path
+
+
+class ClearVoiceProcessor(ModelBackedProcessor):
+    def patch_checkpoint_dir(self, cache_root: Path):
+        try:
+            from clearvoice import network_wrapper
+        except ImportError as e:
+            logger.error("ClearVoice is not found, using `pip install clearvoice`")
+            raise AudioProcessingError from e
+
+        def wrap_loader(loader):
+            def inner(self):
+                loader(self)
+                model_name = getattr(self, "model_name", None)
+                target_dir = cache_root / model_name
+                target_dir.mkdir(parents=True, exist_ok=True)
+                # checkpoint_dir expects a string path
+                self.args.checkpoint_dir = str(target_dir.expanduser().resolve())
+
+            return inner
+
+        for name in ["load_args_se", "load_args_ss", "load_args_sr", "load_args_tse"]:
+            if hasattr(network_wrapper, name):
+                orig = getattr(network_wrapper, name)
+                setattr(network_wrapper, name, wrap_loader(orig))
+
+    def load_model(self):
+        try:
+            from clearvoice import ClearVoice
+        except ImportError as e:
+            logger.error("ClearVoice is not found, using `pip install clearvoice`")
+            raise AudioProcessingError from e
+
+        self.patch_checkpoint_dir(cache_root=config.model_dir)
+        cv = ClearVoice(task=self.task, model_names=[self.model])
+
+        return cv
+
+    def process_single(
+        self, input_path: Path, output_path: Path | None = None, **kwargs
+    ) -> Path:
+        """Speech processing using ClearVoice.
+        learn more: https://github.com/modelscope/ClearerVoice-Studio
+        """
+        self.validate_input(input_path)
+        output_path = output_path or input_path
+
+        self.model(input_path=input_path, online_write=True, output_path=output_path)
+
+        return output_path or input_path
+
+
+@register_processor("audio_denoiser_frcrn16k")
+class Denoiser_FRCRN_16k(ClearVoiceProcessor):
+    task = "speech_enhancement"
+    model = "FRCRN_SE_16K"
